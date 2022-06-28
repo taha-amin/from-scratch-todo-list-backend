aee140d805739ded8ba697efcec0793943521537
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-28 14:14:48 -0700

    in users.test.js created mockUser with UserService

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,28 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: 'test@example.com',
+  password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
